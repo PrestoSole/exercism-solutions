7a714021a62b63892e46be0d17c1a0beab9334e5
PrestoSole <yalather70@pm.me>
2022-08-24 21:04:03 +0300

    solution for raindrops

---
+++ dart/raindrops/lib/raindrops.dart
@@ -0,0 +1,11 @@
+class Raindrops {
+  String convert(int number) {
+    String result = '';
+
+    if (number % 3 == 0) result += 'Pling';
+    if (number % 5 == 0) result += 'Plang';
+    if (number % 7 == 0) result += 'Plong';
+
+    return result == '' ? number.toString() : result;
+  }
+}
